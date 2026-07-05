24fb104a70c751ff7301d00a9c7ac2f53b99b223
KrisMg <kristoffmg200@gmail.com>
2026-07-05 16:49:04 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,691 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fresnillo-1",
+      "name": "FRESNILLO 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05175674377089834,
+          "pitch": 0.27026209640936827,
+          "rotation": 0,
+          "target": "1-fresnillo-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-fresnillo-2",
+      "name": "FRESNILLO 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9799243921125615,
+          "pitch": 0.3257623763284876,
+          "rotation": 0,
+          "target": "0-fresnillo-1"
+        },
+        {
+          "yaw": -1.957152048911265,
+          "pitch": 0.6823870105066661,
+          "rotation": 0,
+          "target": "2-fresnillo-3"
+        },
+        {
+          "yaw": -0.23848301568036945,
+          "pitch": 0.5032010842897598,
+          "rotation": 0,
+          "target": "4-fresnillo-5"
+        },
+        {
+          "yaw": 1.6693398628473695,
+          "pitch": 0.4814569676093008,
+          "rotation": 0,
+          "target": "6-fresnillo-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-fresnillo-3",
+      "name": "FRESNILLO 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9560972203129916,
+          "pitch": 0.6667267244373356,
+          "rotation": 0,
+          "target": "1-fresnillo-2"
+        },
+        {
+          "yaw": -1.1238227149364874,
+          "pitch": 0.5326450290882079,
+          "rotation": 0,
+          "target": "3-fresnillo-4"
+        },
+        {
+          "yaw": 1.932270324249231,
+          "pitch": 0.4309723872064559,
+          "rotation": 0,
+          "target": "4-fresnillo-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-fresnillo-4",
+      "name": "FRESNILLO 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8643815365045215,
+          "pitch": 0.5577960399924571,
+          "rotation": 0,
+          "target": "2-fresnillo-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-fresnillo-5",
+      "name": "FRESNILLO 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.78805343797527,
+          "pitch": 0.5216040777938389,
+          "rotation": 0,
+          "target": "1-fresnillo-2"
+        },
+        {
+          "yaw": -2.748900014461638,
+          "pitch": 0.42147432471878865,
+          "rotation": 0,
+          "target": "2-fresnillo-3"
+        },
+        {
+          "yaw": 0.40730127534727245,
+          "pitch": 0.5455771600384143,
+          "rotation": 0,
+          "target": "5-fresnillo-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-fresnillo-6",
+      "name": "FRESNILLO 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.013029734510588,
+          "pitch": 0.3766115999410875,
+          "rotation": 0,
+          "target": "4-fresnillo-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-fresnillo-7",
+      "name": "FRESNILLO 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.543237002471578,
+          "pitch": 0.6825205426035819,
+          "rotation": 0,
+          "target": "1-fresnillo-2"
+        },
+        {
+          "yaw": 0.004415579779262657,
+          "pitch": 0.11170376415044281,
+          "rotation": 0,
+          "target": "7-fresnillo-8"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-fresnillo-8",
+      "name": "FRESNILLO 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.622439816009095,
+          "pitch": 0.6875662277507466,
+          "rotation": 0,
+          "target": "6-fresnillo-7"
+        },
+        {
+          "yaw": -0.05811910009669674,
+          "pitch": 0.22127776828796186,
+          "rotation": 0,
+          "target": "8-fresnillo-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-fresnillo-9",
+      "name": "FRESNILLO 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.462748791585577,
+          "pitch": 0.39840410711211405,
+          "rotation": 0,
+          "target": "9-fresnillo-10"
+        },
+        {
+          "yaw": -1.1094220437991442,
+          "pitch": 0.6579997707515588,
+          "rotation": 0,
+          "target": "7-fresnillo-8"
+        },
+        {
+          "yaw": -2.4012037666061268,
+          "pitch": 0.5180178394938348,
+          "rotation": 0,
+          "target": "14-fresnillo-15"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-fresnillo-10",
+      "name": "FRESNILLO 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22400589738911592,
+          "pitch": 0.5473097074457911,
+          "rotation": 0,
+          "target": "10-fresnillo-11"
+        },
+        {
+          "yaw": 0.2722606829609546,
+          "pitch": 0.5589205448115653,
+          "rotation": 0,
+          "target": "11-fresnillo-12"
+        },
+        {
+          "yaw": 2.793062151187213,
+          "pitch": 0.7887092457857605,
+          "rotation": 0,
+          "target": "12-fresnillo-13"
+        },
+        {
+          "yaw": 1.048248839099296,
+          "pitch": 0.8312696716953933,
+          "rotation": 0,
+          "target": "13-fresnillo-14"
+        },
+        {
+          "yaw": -3.084416643759665,
+          "pitch": 0.43126959607266,
+          "rotation": 0,
+          "target": "8-fresnillo-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-fresnillo-11",
+      "name": "FRESNILLO 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9537243220988216,
+          "pitch": 0.5141243260556294,
+          "rotation": 0,
+          "target": "9-fresnillo-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-fresnillo-12",
+      "name": "FRESNILLO 12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9679741978990712,
+          "pitch": 0.545873306049506,
+          "rotation": 0,
+          "target": "9-fresnillo-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-fresnillo-13",
+      "name": "FRESNILLO 13",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0452876834742106,
+          "pitch": 0.6619019518162261,
+          "rotation": 0,
+          "target": "9-fresnillo-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-fresnillo-14",
+      "name": "FRESNILLO 14",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.109573276310618,
+          "pitch": 0.5841852722164589,
+          "rotation": 0,
+          "target": "9-fresnillo-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-fresnillo-15",
+      "name": "FRESNILLO 15",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0302594010783093,
+          "pitch": 0.5399606217449104,
+          "rotation": 0,
+          "target": "8-fresnillo-9"
+        },
+        {
+          "yaw": -1.184420550856487,
+          "pitch": 0.6365409463811744,
+          "rotation": 0,
+          "target": "15-fresnillo-17"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-fresnillo-17",
+      "name": "FRESNILLO 17",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0633644081200586,
+          "pitch": 0.7060806935685786,
+          "rotation": 0,
+          "target": "14-fresnillo-15"
+        },
+        {
+          "yaw": 0.9473482843373553,
+          "pitch": 0.9719844297573559,
+          "rotation": 0,
+          "target": "16-fresnillo-18"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-fresnillo-18",
+      "name": "FRESNILLO 18",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.953917685059862,
+          "pitch": 0.7269244274848461,
+          "rotation": 0,
+          "target": "15-fresnillo-17"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
